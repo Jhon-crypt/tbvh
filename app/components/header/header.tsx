8f7bdff82c7ad3fa48a0859643f6ddf09168da57
Jhon-crypt <johnoladele690@gmail.com>
2023-04-26 12:56:34 +0100

    Made some changes

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,3 +1,6 @@
+import { FaGhost } from "react-icons/fa"
+import { BiLogIn, BiUserCircle } from "react-icons/bi"
+
 export default function Header(){
 
     return (
@@ -8,7 +11,7 @@ export default function Header(){
           <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost btn-circle">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#000000"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                <FaGhost className="text-primary text-2xl" />
               </label>
               <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-white text-black rounded-box w-52">
                 <li><a>Homepage</a></li>
@@ -19,7 +22,9 @@ export default function Header(){
           </div>
           <div className="navbar-center">
             <a className="normal-case text-3xl font-extrabold">
-                <b><span className="text-primary">T</span>
+                <b>
+                    
+                    <span className="text-primary">T</span>
                     <span className="text-secondary">B</span>
                     <span className="text-accent">V</span>
                     <span className="text-neutral">H</span>
@@ -28,12 +33,11 @@ export default function Header(){
           </div>
           <div className="navbar-end">
             <button className="btn btn-ghost btn-circle">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#000000"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+              <BiLogIn className="text-black text-2xl" />
             </button>
             <button className="btn btn-ghost btn-circle">
               <div className="indicator">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#000000"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                <span className="badge badge-xs badge-primary indicator-item"></span>
+                <BiUserCircle className="text-black text-2xl" />
               </div>
             </button>
           </div>
--- app/components/hero/IntroHero.tsx
+++ app/components/hero/IntroHero.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { BiLogIn, BiUserCircle } from "react-icons/bi"
 
 export default function IntroHero() {
 
@@ -29,9 +29,7 @@ export default function IntroHero() {
                                         <button className="py-4 px-6 w-full text-white font-semibold rounded-xl bg-neutral">
                                             <div className="flex flex-wrap justify-center items-center -m-1">
                                                 <div className="w-auto p-1">
-                                                    <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M2.75 3.75C2.75 2.92157 3.42157 2.25 4.25 2.25H6.70943C7.03225 2.25 7.31886 2.45657 7.42094 2.76283L8.5443 6.13291C8.66233 6.48699 8.50203 6.87398 8.1682 7.0409L6.47525 7.88737C7.30194 9.72091 8.77909 11.1981 10.6126 12.0247L11.4591 10.3318C11.626 9.99796 12.013 9.83767 12.3671 9.9557L15.7372 11.0791C16.0434 11.1811 16.25 11.4677 16.25 11.7906V14.25C16.25 15.0784 15.5784 15.75 14.75 15.75H14C7.7868 15.75 2.75 10.7132 2.75 4.5V3.75Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                    </svg>
+                                                    <BiLogIn />
                                                 </div>
                                                 <div className="w-auto p-1">
                                                     <span>Login Now</span>
@@ -42,12 +40,10 @@ export default function IntroHero() {
                                 </div>
                                 <div className="w-full md:w-auto p-2.5">
                                     <div className="block">
-                                        <button className="py-4 px-9 w-full font-semibold rounded-xl bg-secondary " type="button">
+                                        <button className="py-4 px-9 w-full font-semibold rounded-xl bg-secondary text-white" type="button">
                                             <div className="flex flex-wrap justify-center items-center -m-1">
                                                 <div className="w-auto p-1">
-                                                    <svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M2.75 3.75C2.75 2.92157 3.42157 2.25 4.25 2.25H6.70943C7.03225 2.25 7.31886 2.45657 7.42094 2.76283L8.5443 6.13291C8.66233 6.48699 8.50203 6.87398 8.1682 7.0409L6.47525 7.88737C7.30194 9.72091 8.77909 11.1981 10.6126 12.0247L11.4591 10.3318C11.626 9.99796 12.013 9.83767 12.3671 9.9557L15.7372 11.0791C16.0434 11.1811 16.25 11.4677 16.25 11.7906V14.25C16.25 15.0784 15.5784 15.75 14.75 15.75H14C7.7868 15.75 2.75 10.7132 2.75 4.5V3.75Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                    </svg>
+                                                    <BiUserCircle className="text-white-focus"/>
                                                 </div>
                                                 <div className="w-auto p-1 text-white">
                                                     <span>Signup Now</span>
