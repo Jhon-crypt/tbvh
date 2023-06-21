e5c07e4a0f1d5df3ae33ac6a4c14126d10eb81cf
Jhon-crypt <johnoladele690@gmail.com>
2023-06-21 15:12:32 +0100

    Made some changes

--- app/api/chatbox/fetchChatBoxes/route.ts
+++ app/api/chatbox/fetchChatBoxes/route.ts
@@ -1,3 +1,23 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
+import { NextResponse } from "next/server";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+export async function GET(request: Request) {
+
+  const supabase = createServerComponentClient({ cookies });
+
+  //fetching user UUID from supbase
+  const { data }: any = await supabase.auth.getUser();
+
+  if (data) {
+    let { data: honestBox, error }: any = await supabase
+      .from("honest_box")
+      .select("*")
+      .eq("user_uuid", `${data.user.id}`)
+      .order("id", { ascending: false });
+
+    return NextResponse.json({ honestBox });
+  }else{
+    return NextResponse.json({ "message": 'Error during Request' });
+  }
+}
