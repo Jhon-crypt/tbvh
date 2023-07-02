626f58b11f2a12f02eb82e1473e0d9ccfb002063
Jhon-crypt <johnoladele690@gmail.com>
2023-07-02 01:52:04 +0100

    Made a change in the chat section

--- app/components/section/chatSection.tsx
+++ app/components/section/chatSection.tsx
@@ -91,7 +91,7 @@ export default function ChatSection(props:
                     <div className="flex bg-neutral-content rounded-lg p-4 mb-7 text-sm text-neutral-focus" role="alert">
                         <svg className="w-5 h-5 inline mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
                         <div>
-                            <span className="font-medium">Copy Link : http://localhost:3000/chat/9bd10ad4-a26f-4c0a-93be-cfbc561e661e</span>
+                            <span className="font-medium">Copy this : Comment here https://tbvh.vercel.app/chat/9bd10ad4-a26f-4c0a-93be-cfbc561e661e</span>
                         </div>
                     </div>
 
